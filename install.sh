5e96efede308b4ba501c799008bbbffb8a19855f
Federico Berto <berto.federico2@gmail.com>
2021-05-03 21:05:25 +0900

    Add MacOS installer

--- install.sh
+++ install.sh
@@ -1,29 +1,74 @@
 #!/bin/bash
 # Installer script for ARCSim 0.3.1 on Ubuntu and other Debian-based distributions
+	
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
+UNAME=$(uname) # get the operating system
 
-echo -e "${GREEN}Would you like to install the required packages for Ubuntu / Debian-based distributions?${NC}"
-select yn in "Yes" "No"
-do
-	case $yn in
-		Yes) echo "Installing necessary packages..."; apt-get update; 
-			apt-get install gcc make g++ libboost-all-dev freeglut3-dev \
-				gfortran liblapacke-dev libpng-dev libpng++-dev scons \
-        			libatlas-base-dev ctags libopenblas-dev; break;;
-		No) echo "Skipping..."; break;;
-	esac
+# Parsing arguments
+while (( "$#" )); do
+    case "$1" in
+        -o|--os)
+            if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+                UNAME=$2
+                shift 2
+            else
+                echo "Error: Argument for $1 is missing" >&2
+                exit 1
+            fi
+            ;;
+        -h|--help)
+            echo "Usage:  $0 -o <input> [options]" >&2
+            echo "        -o | --os  %  (Operating System Name: Linux or Darwin (MacOS))" >&2
+            exit 0
+            ;;
+        -*|--*) # unsupported flags
+            echo "Error: Unsupported flag: $1" >&2
+            echo "$0 -h for help message" >&2
+            exit 1
+            ;;
+        *)
+            echo "Error: Arguments with not proper flag: $1" >&2
+            echo "$0 -h for help message" >&2
+            exit 1
+            ;;
+    esac
 done
+echo "Operating system: ${UNAME}"
+MAKEFILE="Makefile.linux"
+if [ "$UNAME" = "Darwin" ]; then
+	MAKEFILE="Makefile.mac"
+elif [ "$UNAME" = "Linux" ]; then
+	:
+else
+	echo -e "${ORANGE}WARNING: unsupported OS parsed. Possible options are Linux and Darwin (MacOS). Running Linux script as default...${NC}"
+fi
+
+
+if [ "$UNAME" = "Linux" ]; then
+	echo -e "${GREEN}Would you like to install the required packages for Ubuntu / Debian-based distributions?${NC}"
+	select yn in "Yes" "No"
+	do
+		case $yn in
+			Yes) echo "Installing necessary packages..."; apt-get update; 
+				apt-get install gcc make g++ libboost-all-dev freeglut3-dev \
+					gfortran liblapacke-dev libpng-dev libpng++-dev scons \
+					libatlas-base-dev ctags libopenblas-dev; break;;
+			No) echo "Skipping..."; break;;
+		esac
+	done
+else
+	echo -e "${ORANGE}WARNING: you firstly may need to install the necessary packages for your OS as described in the INSTALL file / install.sh script!${NC}"
+fi
 
 cd dependencies/
 echo "Running make on dependencies folder. Grab a cup of coffee, this will take a while."
 make
-if [ $? -ne 0 ]
-then
-	echo -e "${RED}Make error on dependencies occurred. Exiting...${NC}"
+if [ $? -ne 0 ]; then
+	echo -e "${RED}ERROR: Make error on dependencies occurred. Exiting...${NC}"
 	exit 1
 fi
 
@@ -35,10 +80,10 @@ sed -i '805s|.*|/*extern int isnan(double);*/|' dependencies/include/taucs.h
 sed -i '811s|.*|/*extern int isinf(double);*/|' dependencies/include/taucs.h
 
 echo "Running make on main folder. This will also take a while."
-make -f "Makefile.linux"
-if [ $? -ne 0 ]
-then
-	echo -e "${RED}Make error on main folder occurred. Exiting...${NC}"
+
+make -f $MAKEFILE
+if [ $? -ne 0 ]; then
+	echo -e "${RED}ERROR: Make error on main folder occurred. Exiting...${NC}"
 	exit 1
 fi
 
@@ -52,4 +97,3 @@ do
 	esac
 done
 echo "Exiting script..."
-
